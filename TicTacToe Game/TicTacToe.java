dce94c338a29af26ca10fa081975fc1726e79388
kaushalsingh585 <kaushalsingh584@gmail.com>
2023-02-02 12:50:10 +0530

    added TicTacToe

---
+++ TicTacToe Game/TicTacToe.java
@@ -0,0 +1,49 @@
+package com.monocept.model;
+
+public class TicTacToe {
+	
+	private char[][] arr;
+	
+	public TicTacToe()
+	{
+		arr= new char[3][3];
+	}
+	
+
+	public char[][] getArr() {
+		return arr;
+	}
+
+	public void setArr(char[][] arr,int row,int col,char ch) {
+		arr[row - 1][col - 1] = ch;
+	}
+	public int checkwinner(char[][] arr, char ch, int turn) {
+		// TODO Auto-generated method stub
+		//scanning rows
+		if ((arr[0][0] == ch && arr[0][1] == ch && arr[0][2] == ch)
+				|| (arr[1][0] == ch && arr[1][1] == ch && arr[1][2] == ch)
+				|| (arr[2][0] == ch && arr[2][1] == ch && arr[2][2] == ch))
+			return 0;
+		
+		//scanning columns
+		if (arr[0][0] == ch && arr[1][0] == ch && arr[2][0] == ch
+				|| (arr[0][1] == ch && arr[1][1] == ch && arr[2][1] == ch)
+				|| (arr[0][2] == ch && arr[1][2] == ch && arr[2][2] == ch))
+			return 0;
+		
+		//scanning right diagonal
+		if (arr[0][0] == ch && arr[1][1] == ch && arr[2][2] == ch)
+			return 0;
+		
+		//scanning left diagonal
+		if (arr[0][2] == ch && arr[1][1] == ch && arr[2][0] == ch)
+			return 0;
+
+		if (turn == 9)
+			return -1;
+		
+		return 1;
+	}
+	
+
+}
---
+++ TicTacToe Game/TicTacToeGameTest.java
@@ -0,0 +1,70 @@
+package com.monocept.test;
+
+import java.util.Scanner;
+
+import com.monocept.model.TicTacToe;
+
+public class TicTacToeGameTest {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		char[][] arr ;
+
+		char p1 = 'O', p2 = 'X', ch = 'O';
+		int turn = 0;
+		
+		TicTacToe obj = new TicTacToe();
+
+		int flag = 1;
+
+		while (flag == 1) {
+
+			
+			System.out.println("\nPlayer  " +ch+": enter the row and column:");
+
+			int row = sc.nextInt();
+			int col = sc.nextInt();
+			arr = obj.getArr();
+
+			if (arr[row - 1][col - 1] != '\u0000')
+				{
+				System.out.println("Position already occupied ! Please enter other position");
+				continue;
+				}
+			
+			else {
+				turn++;
+				obj.setArr(arr, row, col, ch);
+			}
+			
+			System.out.println("---------- ");
+			for (int i = 0; i < arr.length; i++) {
+
+				System.out.println(arr[i][0] + " | " + arr[i][1] + " | " + arr[i][2]);
+				System.out.println("---------- ");
+
+			}
+
+			flag = obj.checkwinner(arr, ch, turn);
+
+			if (flag == -1) {
+				System.out.println("Draw");
+				break;
+			} else if (flag == 0) {
+				System.out.println("Player "+ch+" Won!!");
+				break;
+			}
+			
+			if(ch == 'O')
+				ch = 'X';
+			else {
+				ch = 'O';
+			}
+			
+			
+		}
+
+	}
+
+}
