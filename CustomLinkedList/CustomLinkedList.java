3b5430d87244261320abe111c56f1b635e395e96
kaushalsingh585 <kaushalsingh584@gmail.com>
2023-02-18 15:54:48 +0530

    added updated CustomLinkedList.java

--- CustomLinkedList/CustomLinkedList.java
+++ CustomLinkedList/CustomLinkedList.java
@@ -1,26 +1,78 @@
 package com.monocept.model;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class CustomLinkedList {
 
-	private LinkedList<Integer> linkedList;
-
-	public CustomLinkedList() {
-		super();
-		this.linkedList = new LinkedList<Integer>();
-	}
+	Node node;
 
 	public void add(int num) {
-		linkedList.add(num);
+		Node newNode = new Node(num);
+		if (node == null)
+			node = newNode;
+		else {
+			Node temNode = node;
+			while (temNode != null && temNode.getNext() != null) {
+
+				temNode = temNode.getNext();
+			}
+			temNode.setNext(newNode);
+		}
 	}
 
-	public void remove(Integer num) {
-		linkedList.remove(num);
+	public void display() {
+		Node tempNode = node;
+		while (tempNode != null) {
+			System.out.print(tempNode.getData() + " ");
+			tempNode = tempNode.getNext();
+		}
+		System.out.println();
+
 	}
 
-	public LinkedList<Integer> display() {
-		return linkedList;
+	public void remove(int num) {
+
+		// checking if the list is empty
+		if (node == null) {
+			System.out.println("Underflow: LinkedList is empty");
+			return;
+		}
+
+		// if the element is present as the first node
+		if (node.getData() == num)
+			node = node.getNext();
+		// if the element is present in mid or last
+		else {
+			Node tempNode = node;
+			int flag = 0;
+			// checks if the element is last
+			while (tempNode != null && tempNode.getNext() != null) {
+				if (tempNode.getNext().getData() == num) {
+					if (tempNode.getNext().getNext() != null)
+						tempNode.setNext(tempNode.getNext().getNext());
+					else
+						tempNode.setNext(null);
+					flag = 1;
+					break;
+				}
+				tempNode = tempNode.getNext();
+			}
+			if (flag == 0)
+				System.out.println("element does not exist");
+
+		}
+
+	}
+
+	public ArrayList<Integer> iterate() {
+
+		ArrayList<Integer> ll = new ArrayList<Integer>();
+		Node tempNode = node;
+		while (tempNode != null) {
+			ll.add(tempNode.getData());
+			tempNode = tempNode.getNext();
+		}
+		return ll;
 	}
 
 }
--- CustomLinkedList/CustomLinkedListTest.java
+++ CustomLinkedList/CustomLinkedListTest.java
@@ -1,56 +1,37 @@
 package com.monocept.test;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 import com.monocept.model.CustomLinkedList;
+import com.monocept.model.Node;
 
 public class CustomLinkedListTest {
 
 	public static void main(String[] args) {
-
+		
 		CustomLinkedList list = new CustomLinkedList();
-		Scanner scanner = new Scanner(System.in);
-		boolean flag = true;
-		while (flag) {
-			System.out.println("Enter 1 to add value to LinkedList");
-			System.out.println("Enter 2 to remove value to LinkedList");
-			System.out.println("Enter 3 to display value of LinkedList");
-			System.out.println("Enter 4 to iterate through LinkedList");
-			System.out.println("Enter 5 to end");
-			System.out.println("\nEnter the response");
-			int choice = scanner.nextInt();
-			
-			switch (choice) {
-			case 1:
-				System.out.println("Enter value to add to the linkedList");
-				int num = scanner.nextInt();
-				list.add(num);
-				break;
-			case 2:
-				System.out.println("Enter value to remove element from the linkedList");
-				list.remove(scanner.nextInt());
-
-				break;
-			case 3:
-				System.out.println(list.display());
-				break;
-			case 4:
-				LinkedList<Integer> temporaryList = list.display();
-				for(int item :temporaryList)
-					 System.out.println(item);
-				break;
-			case 5:
-				flag = false;
-				System.out.println("End !!!");
-				break;
-
-			default:
-				System.out.println("Invalid choice");
-				break;
-			}
-		}
+		
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		list.add(50);
+		list.add(60);
+		list.add(10);
+		System.out.println("Elements of the CustomLinkedList after adding are: ");
+		list.display();
+		
+		list.remove(30);
+		list.remove(10);
+		System.out.println("Elements of the CustomLinkedList after removing 30 and 10 are: ");
+		list.display();
+		
+		System.out.println("Elements of the CustomLinkedList after interating through arraylist: ");
+		ArrayList<Integer>items = list.iterate();
+		for(Integer item : items)
+			System.out.println(item);
+		
+		
 		
 	}
 
